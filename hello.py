2b929478991622ccc8a3b0be9b6cd70a4aba6a82
Matthijs Raatgever <matthijs4004@gmail.com>
2021-09-06 12:14:01 +0200

    foutmelding

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 
 print("Hello wereld van…")                                                                       
 print("pppp    y     y   ttttttt   h   h    oooo   nnnn")          
-prin("p   p    y   y       t      h   h   o    o  n   n")
+print("p   p    y   y       t      h   h   o    o  n   n")
 print("p   p     y y        t      h   h   o    o  n   n")
 print("pppp       y         t      hhhhh   o    o  n   n")
 print("p          y         t      h   h   o    o  n   n")
